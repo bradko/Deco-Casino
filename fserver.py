f3519a1b6791c39e524cc3b27e89ce675cc5b06f
bradko <kovabr01@luther.edu>
2018-05-13 20:09:16 -0500

    got login to display username, improved styles, worked with credits table

--- fserver.py
+++ fserver.py
@@ -13,23 +13,28 @@ def index():
 	if not session.get('logged_in'):
 		return render_template('login.html')
 	else:
-		return render_template('index.html')
+		return render_template('index.html', username=session['username'], credits=session['credits'])
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
  
-    POST_USERNAME = str(request.form['username'])
-    POST_PASSWORD = str(request.form['password'])
- 
-    Session = sessionmaker(bind=engine)
-    s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-    result = query.first()
-    if result:
-        session['logged_in'] = True
-    else:
-        flash('Invalid Credentials, Please Try Again')
-    return index()
+	POST_USERNAME = str(request.form['username'])
+	POST_PASSWORD = str(request.form['password'])
+
+	Session = sessionmaker(bind=engine)
+	s = Session()
+	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+	result = query.first()
+	if result:
+		query = s.query(Base.metadata.tables['credits']).filter(User.username.in_([POST_USERNAME]))
+		result = query.first()
+		print(result)
+		session['credits'] = result[1]
+		session['username'] = POST_USERNAME
+		session['logged_in'] = True
+	else:
+	    flash('Invalid Credentials, Please Try Again')
+	return index()
 
 @app.route("/logout")
 def logout():
@@ -50,12 +55,19 @@ def addToLogin():
 	Session = sessionmaker(bind=engine)
 	session = Session()
 
-	user = User(username, password)
-	session.add(user)
+	if session.query(exists().where(User.username == username)).scalar():
+		flash('Username Already Exists, Please Try Another')
+		return createAccount()
+	else:
+		user = User(username, password)
+		session.add(user)
 
-	session.commit()
+		credits = Credits(username, 1000)
+		session.add(credits)
 
-	return index()
+		session.commit()
+
+		return index()
 
 if __name__ == '__main__':
 	app.secret_key = os.urandom(12)
